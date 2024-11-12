4c60985638223d6a512e28d38bc559f0d2e68914
hans-repo <email.hanss@proton.me>
2024-11-12 12:21:26 +1100

    run python script using linux &> for output

--- runGCP.py
+++ runGCP.py
@@ -3,12 +3,13 @@ import subprocess
 import logging
 import sys
 import threading
+import os
 
 
-nrExperiments = 40
+nrExperiments = 2
 protocolName = "gcp"
 nodes = 0  # Number of nodes
-time = 600  # Number of seconds to run the experiment
+time = 10  # Number of seconds to run the experiment
 batchSize = 1 #62500 for 500KB of transactions sized 8B each, Narwhal's batch size
 crashes = 0
 host = "192.168.0.115"
@@ -20,6 +21,7 @@ for i in range(nrExperiments):
     # crashes = nodes // 4
     # batchSize = batchSize + 50
     # Command to run the Haskell program using cabal
+    log_file = f"output_{i}.log"
     command = [
         "cabal", "v2-run", protocolName, "--",
         "--replicas", str(nodes),
@@ -28,29 +30,32 @@ for i in range(nrExperiments):
         "--batchSize", str(batchSize),
         "--host", str(host),
         "--port", str(port),
-        "-m", "master"
+        "-m", "master",
+        "&> ", log_file
     ]
+    command_string = ' '.join(command) + f" &> {log_file}"
+    os.system(command_string)
 
-    # Setup logger for the current experiment
-    # logger = logging.basicConfig(filename=f"output_{i}.log", level=logging.INFO)
-    logger = logging.getLogger(f"experiment_{i}")
-    logger.setLevel(logging.INFO)
+    # # Setup logger for the current experiment
+    # # logger = logging.basicConfig(filename=f"output_{i}.log", level=logging.INFO)
+    # logger = logging.getLogger(f"experiment_{i}")
+    # logger.setLevel(logging.INFO)
 
 
-    process = Popen(command, stdout=PIPE, stderr=STDOUT)
-    with process.stdout:
-        log_file = f"output_{i}.log"
-        log_format = "|%(levelname)s| : [%(filename)s]--[%(funcName)s] : %(message)s"
-        formatter = logging.Formatter(log_format)
+    # process = Popen(command, stdout=PIPE, stderr=STDOUT)
+    # with process.stdout:
+    #     log_file = f"output_{i}.log"
+    #     log_format = "|%(levelname)s| : [%(filename)s]--[%(funcName)s] : %(message)s"
+    #     formatter = logging.Formatter(log_format)
 
-        # create file handler and set the formatter
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setFormatter(formatter)
+    #     # create file handler and set the formatter
+    #     file_handler = logging.FileHandler(log_file)
+    #     file_handler.setFormatter(formatter)
 
-        # add handler to the logger
-        logger.addHandler(file_handler)
-        for line in iter(process.stdout.readline, b''): # b'\n'-separated lines
-            logger.info('%r', line)
-    exitcode = process.wait() # 0 means success
-    if exitcode == 0:
-        print(f"processed experiment {i}")
+    #     # add handler to the logger
+    #     logger.addHandler(file_handler)
+    #     for line in iter(process.stdout.readline, b''): # b'\n'-separated lines
+    #         logger.info('%r', line)
+    # exitcode = process.wait() # 0 means success
+    # if exitcode == 0:
+    print(f"processed experiment {i}")
