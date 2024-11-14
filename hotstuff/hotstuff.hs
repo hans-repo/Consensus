479974e718c78c339c9d234faaecf60784fe9edb
hans-repo <email.hanss@proton.me>
2024-11-15 04:31:49 +1100

    tick time 10^5 hotstuff

--- hotstuff/hotstuff.hs
+++ hotstuff/hotstuff.hs
@@ -273,7 +273,7 @@ spawnServer batchSize clientPid = spawnLocal $ do
 
     serverPids <- expect
     say $ "received servers " ++ show serverPids
-    let tickTime = 1*10^4
+    let tickTime = 1*10^5
         timeoutMicroSeconds = 10*10^5
         timeoutTicks = timeoutMicroSeconds `div` tickTime
     say $ "synchronous delta timers set to " ++ show timeoutTicks ++ " ticks"
@@ -306,7 +306,7 @@ spawnClient batchSize nSlaves replicas crashCount = spawnLocal $ do
     mapM_ (`kill` "crash node") toCrashNodes
     say $ "sent crash command to " ++ show toCrashNodes
   
-    let tickTime = 1*10^4
+    let tickTime = 1*10^5
     spawnLocal $ forever $ do
         liftIO $ threadDelay tickTime
         currentTime <- liftIO getPOSIXTime
