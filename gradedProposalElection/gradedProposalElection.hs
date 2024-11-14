cbb5fa6e46ca06df8ee62bd510f7ec49400ffd66
hans-repo <email.hanss@proton.me>
2024-11-15 00:43:09 +1100

    use all lastdelivered for gradedproposlelection for latency

--- gradedProposalElection/gradedProposalElection.hs
+++ gradedProposalElection/gradedProposalElection.hs
@@ -112,7 +112,7 @@ tickClientHandler (ClientTick tickTime) = do
             -- let truncLastDelivered = V.drop ((V.length lastDeliveredOld) - cmdRate) lastDeliveredOld
             let truncLastDelivered = lastXElements cmdRate lastDeliveredOld
             lastDelivered .= truncLastDelivered
-            currLatency .= (meanTickDifference truncLastDelivered (round $ tickTime*10^6)) / 10^6
+            currLatency .= (meanTickDifference lastDelivered (round $ tickTime*10^6)) / 10^6
         else do
             let truncLastDelivered = lastDeliveredOld
             -- currLatency .= (meanTickDifference truncLastDelivered (round $ tickTime*10^6)) / 10^6
